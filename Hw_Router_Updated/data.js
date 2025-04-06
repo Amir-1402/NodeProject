ce8ef153a9bc551f8dcb770d412453f2144b9f9c
Amir-1402 <amir.fa1402@gmail.com>
2025-04-06 23:03:48 +0300

    Add files via upload

---
+++ Hw_Router_Updated/data.js
@@ -0,0 +1,45 @@
+const data = {
+  products: [
+    { id: 101, name: "Smartwatch", price: 250 },
+    { id: 102, name: "Wireless Charger", price: 60 },
+    { id: 103, name: "Bluetooth Speaker", price: 120 },
+    { id: 104, name: "Gaming Mouse", price: 75 },
+    { id: 105, name: "External SSD", price: 180 },
+  ],
+  users: [
+    {
+      id: 11,
+      name: "Amir Fahmawi",
+      email: "Amir.Fahmawi@example.com",
+      age: 24,
+      city: "Tel Aviv",
+      isActive: true,
+    },
+    {
+      id: 12,
+      name: "Amir Fah",
+      email: "Amir.Fah@example.com",
+      age: 29,
+      city: "Haifa",
+      isActive: false,
+    },
+    {
+      id: 13,
+      name: "Alex Alex",
+      email: "Alex.Alex@example.com",
+      age: 32,
+      city: "Jerusalem",
+      isActive: true,
+    },
+    {
+      id: 14,
+      name: "Smith Smith",
+      email: "Smith.fa@example.com",
+      age: 27,
+      city: "Beer Sheva",
+      isActive: true,
+    },
+  ],
+};
+
+module.exports = data;
